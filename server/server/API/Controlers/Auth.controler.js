cb030cc58d66f2a8fe217cd9c573481ea58cedd0
AshuYadav2004 <ashutoshyadav81787@gmail.com>
2024-01-23 15:46:08 +0530

    last fixed

--- server/server/API/Controlers/Auth.controler.js
+++ server/server/API/Controlers/Auth.controler.js
@@ -1,9 +1,13 @@
-function SignUP(){
-
+function SignUP(req,res){
+  res.send('working')
 }
 
-function SignIn(){
-    
+function SignIn(req,res){
+    res.send("hello working")
 }
 
-module.exports = {SignIn,SignUP}
+function logout(req,res){
+    res.send("hello working")
+}
+
+module.exports = {SignIn,SignUP,logout}
--- server/server/routes/index.js
+++ server/server/routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
-const {SignIn,SignUP} = require('../API/Controlers/Auth.controler')
+const {SignIn,SignUP,logout} = require('../API/Controlers/Auth.controler')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/sig-nup',SignUP);
-router.post('sign-in',SignIn)
+router.get('/sign-up',SignUP);
+router.get('/sign-in',SignIn);
+router.get('/logout',logout);
 
 module.exports = router;
