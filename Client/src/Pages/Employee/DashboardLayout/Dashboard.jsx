01531de6cb4ffffd524cb28fca3e719eaa14dea5
vipul-semwal <vipulsemwa124@gmail.com>
2024-02-09 04:21:10 +0530

    added profile pic feature

--- Client/src/Pages/Employee/DashboardLayout/Dashboard.jsx
+++ Client/src/Pages/Employee/DashboardLayout/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Dashboard.css'
-import { ProfileCard,Employeetab } from '../../../Components/export'
+import { ProfileCard,Employeetab, PersonalDetails } from '../../../Components/export'
 import {Loader} from '../../../Components/export';
 import employee from '../../../API/Employee';
 import { ToastContainer, toast } from 'react-toastify';
@@ -9,6 +9,7 @@ import { useState,useEffect } from 'react';
 function Dashboard({children,navtag}) {
     const [loader,Setloader] = useState(false);
     const [formData, setFormData] = useState({})
+    const [reload,setReload] = useState(false)
     useEffect(()=>{
         (async ()=>{
             Setloader(true)
@@ -23,14 +24,17 @@ function Dashboard({children,navtag}) {
            toast.error("erro fetching data")
          }
         })()
-      },[])
+      },[reload])
  const currentAddress = (
   (formData?.AdditionalUserinfo?.PersonalDetails ?? {}).CurrentAddress ?? null
 )
 const profileCompleate = formData?.ProfileCompleate
-console.log("formdata",profileCompleate)
+// console.log("CUrrent",currentAddress)
    
-
+const fncForReload = ()=>{
+  setReload(!reload)
+  console.log('bhen ki chut')
+}
 
     if(loader){
         return <Loader style={{ width: '100vw',
@@ -44,9 +48,7 @@ console.log("formdata",profileCompleate)
   <div className="container">
   <ProfileCard email={formData.email || ""} proifePic={formData.profilePicture || ""
 } number={formData.mobile || ""} name={formData.firstName
-} location={currentAddress
-
-|| " "}  compleateProfile={profileCompleate} />
+} location={currentAddress || ""}  compleateProfile={profileCompleate} utlityFunction={fncForReload}/>
 <Employeetab active={navtag}/>
 {children}
   </div>
