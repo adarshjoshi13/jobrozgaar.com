df819ec4dd34773238c548f98e1d8a2661c8b2c1
vipul-semwal <vipulsemwa124@gmail.com>
2024-02-17 05:35:22 +0530

    pusing the code that ashus send progress bar

--- Client/src/Pages/Employer/Dashboard/Job/AddJob/AddJob.jsx
+++ Client/src/Pages/Employer/Dashboard/Job/AddJob/AddJob.jsx
@@ -1,42 +1,40 @@
 import React, { useState } from 'react';
-import { JobPosting, Candidate } from '../../../../../Pages/export'
-import "./AddJob.css";
+import { JobPosting, Candidate } from '../../../../../Pages/export';
+import './AddJob.css';
 
 function AddJob() {
-    const [currentStep, setCurrentStep] = useState(1);
+  const [currentStep, setCurrentStep] = useState(1);
 
-    function nextStep() {
-        setCurrentStep(currentStep + 1);
-    }
+  const handleNextStep = () => {
+    setCurrentStep((prevStep) => prevStep === 1 ? 2 : 1);
+  };
 
-    return (
-        <div className="container p-0">
-            <div className="progresbar">
-                <div className="container-fluid">
-                    <br /><br />
-                    <ul className="list-unstyled multi-steps">
-                        <li className={currentStep === 1 ? 'is-active' : ''}>
-                            Start
-                            <div className="progress-bar progress-bar--success">
-                                <div className="progress-bar__bar"></div>
-                            </div>
-                        </li>
-                        <li className={currentStep === 4 ? 'is-active' : ''}>
-                            Finish
-                        </li>
-                    </ul>
-                    <br />
-                    <br />
-                    {currentStep < 4 && <button onClick={nextStep}>Next Step</button>}
-                </div>
-            </div>
-            <div className="main-forms">
-                {currentStep === 1 ? <JobPosting /> : <Candidate />}
-            </div>
+  return (
+    <div className="container p-0">
+      <div className="progresbar">
+        <div className="container-fluid">
+          <br /><br />
+          <ul className="list-unstyled multi-steps">
+            <li className={currentStep === 1 ? 'is-active' : ''}>
+              Start
+              <div className="progress-bar progress-bar--success">
+                <div className={`progress-bar__bar ${currentStep > 1 ? 'is-complete' : ''}`}></div>
+              </div>
+            </li>
+            <li className={currentStep === 2 ? 'is-active' : ''}>
+              Finish
+            </li>
+          </ul>
+          <br />
+          <br />
+          <button onClick={handleNextStep}>Next Step</button>
         </div>
-    );
+      </div>
+      <div className="main-forms">
+        {currentStep === 1 ? <JobPosting /> : <Candidate />}
+      </div>
+    </div>
+  );
 }
-
-// testing
 
 export default AddJob;
